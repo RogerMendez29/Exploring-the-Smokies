83315a36cab04b380d25f46161965b8f0bf59d91
Roger Mendez <roger61087@gmail.com>
2022-03-08 15:57:55 -0500

    search bar functionality added

--- client/src/pages/Explore.js
+++ client/src/pages/Explore.js
@@ -11,13 +11,30 @@ import {
 import "../css/explore.css";
 
 function Explore({ renderTrails, trails }) {
+  const [search, setSearch] = useState("");
+
+  const searchResults = trails.filter((trail) => {
+    return trail.trail_name.toLowerCase().includes(search.toLowerCase());
+  });
+
+  console.log(searchResults);
+
+  function handleSearch(event) {
+    setSearch(event.detail.value);
+  }
+
   return (
     <IonPage className="home-page">
       <IonContent>
         <div className="page-contents">
-          <IonSearchbar  className="search-bar"placeholder="Search by trail Name"  ></IonSearchbar>
+          <IonSearchbar
+            value={search}
+            className="search-bar"
+            placeholder="Search by trail Name"
+            onIonChange={handleSearch}
+          ></IonSearchbar>
 
-          <div className="trail-container">{renderTrails(trails)}</div>
+          <div className="trail-container">{renderTrails(searchResults)}</div>
         </div>
       </IonContent>
     </IonPage>
